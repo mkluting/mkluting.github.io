71d00d3a91497addc8a8acb24bd8b159aa9712ed
Matt Kluting <mkluting@gmail.com>
2014-11-08 02:43:36 +0000

    changed char image.

--- character.js
+++ character.js
@@ -2,7 +2,7 @@ game.addEventListener("load", function(){
 
   // Get a character
   character = new Sprite(32, 32)
-  character.image = game.assets['images/girl.gif']
+  character.image = game.assets['images/boy.gif']
   character.frame = 0
   character.x = 160
   character.y = 160
